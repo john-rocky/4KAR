6624a085c8c29fbe5fc7e711bce0af05f00744e9
john-rocky <rockyshikoku@gmail.com>
2022-06-20 08:49:34 +0900

    Initial Commit

---
+++ 4KAR/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  4KAR
+//
+//  Created by 間嶋大輔 on 2022/06/20.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
